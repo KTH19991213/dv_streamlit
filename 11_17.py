cfadf76d45499e1cce5ec030a25c41db7d4b18b6
PAINWED KIM <105909466+KTH19991213@users.noreply.github.com>
2025-11-17 15:11:24 +0900

    Update title text in Streamlit app

--- 11_17.py
+++ 11_17.py
@@ -2,8 +2,9 @@ import streamlit as st
 
 #타이틀 텍스트 출력
 
-st.title("안녕하세요! Streamlit! dd")
+st.title("안녕하세요! Streamlit! This is the mother fucking Markdown HEADER")
 
 # "This is the mother fucking Markdown HEADER"
+
 
 
